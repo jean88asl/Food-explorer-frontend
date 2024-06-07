28b7b0829f6906c3443204d7402ac8563976f576
Jean Augusto <jean88asl@gmail.com>
2024-06-07 20:41:04 -0300

    adding routes file

---
+++ src/Router.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { SignIn } from './pages/SignIn'
+import { SignUp } from './pages/SignUp'
+
+export function Router() {
+    return (
+        <Routes>
+            <Route path='/' element={<Home />} /> 
+            <Route path='/signin' element={<SignIn/>} />
+            <Route path='/signup' element={<SignUp/>} />
+        </Routes>
+    )
+}
