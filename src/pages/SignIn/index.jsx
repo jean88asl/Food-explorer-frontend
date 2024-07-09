b4aab733329e688fee0f592af23b2acc29c8e8fd
Jean Augusto <jean88asl@gmail.com>
2024-07-08 21:09:19 -0300

    integration with backend added

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,25 @@
+import { useState } from 'react'
 import { SignInContainer, SignInHeader, SignInForm, SignInLink } from './styles'
 import Logo from "../../assets/logo.svg"
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
+import { useAuth } from "../../hooks/auth"
+
 
 export function SignIn() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const { signIn } = useAuth()
+
+    function handleSignIn() {
+        if (!email || !password) {
+            alert("Favor preencha todos os campos")
+        }
+
+        signIn({ email, password })
+    }
+
     return (
         <SignInContainer>
             <SignInHeader>
@@ -16,19 +32,24 @@ export function SignIn() {
                 <div>
                     <label htmlFor="">Email</label>
                     <Input
+                        required
                         placeholder='Exemplo:exemplo@email.com.br'
+                        type="email"
+                        onChange={e => setEmail(e.target.value)}
                     />
                 </div>
 
                 <div>
                     <label htmlFor="">Senha</label>
                     <Input
+                        required
                         type="password"
-                        placeholder='No mínimo 6 caracteres' 
+                        placeholder='No mínimo 6 caracteres'
+                        onChange={e => setPassword(e.target.value)}
                     />
                 </div>
 
-                <Button title="Entrar"/>
+                <Button title="Entrar" type="button" onClick={handleSignIn} />
 
                 <SignInLink href="#">Criar uma conta</SignInLink>
             </SignInForm>
